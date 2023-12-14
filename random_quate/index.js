5a08955d426619cdb5f98873261d0c78eb8bc506
Pallavi Dhakne <85501739+pallavidhakne@users.noreply.github.com>
2023-12-14 14:37:44 +0530

    random_quote application

---
+++ random_quate/index.js
@@ -0,0 +1,22 @@
+const array=[
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+    "The way to get started is to quit talking and begin doing. -Walt Disney",
+    "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
+    "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+    "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+    "You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one. -John Lennon",
+    "Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa",
+    "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller",
+    "It is during our darkest moments that we must focus to see the light. -Aristotle"
+]
+const currval=document.getElementById("quoteDisplay");
+let newval=currval;
+function newQuote(){
+    let x=Math.floor(Math.random()*10);
+    alert("new quote is displying!");
+    currval.innerHTML=array[x];
+}
+
+
+
